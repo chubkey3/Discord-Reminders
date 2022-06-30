f5ecea5adaefec02a68e64954a27b13519a21d2f
Jason Louie <jason.jw.louie@gmail.com>
2022-06-29 21:46:56 -0700

    reminder features work

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 const {Client} = require('discord.js');
+const schedule = require('node-schedule');
+
+
+const jobs = []
 
 require('dotenv').config()
 
@@ -21,7 +25,7 @@ client.on('messageCreate', (message) => {
 
             let a = message.content.split(' ');
 
-            if (a.length !== 3){
+            if (a.length < 3 || a.length > 4){
                 message.channel.send('Please use the proper format for this command\nIf you need help, message !h to get the list of commands')
 
             } else {
@@ -32,9 +36,76 @@ client.on('messageCreate', (message) => {
                 } else {
                     tasks[id][a[1]] = a[2]
                     message.channel.send('Task Added!')
+                    let date;
+                    if (a.length === 3){
+                        let today = new Date()
+                        date = new Date(today.getFullYear(), today.getMonth(), today.getDate(), a[2].split(':')[0], a[2].split(':')[1].replace('0', ''))
+                        
+
+                    } else {
+                        date = new Date(a[3].split('/')[2], a[3].split('/')[0]-1, a[3].split('/')[1], a[2].split(':')[0], a[2].split(':')[1].replace('0', ''))
+
+                    }
+                   
+                    jobs.push(schedule.scheduleJob(date, function(){
+                        message.channel.send(`<@${message.author.id}>`)
+                        message.channel.send(`Task: **${a[1]}**`)
+                    }))
+                    
                 }
             }
         }
+        
+        else if (message.content.startsWith('!as ')) {
+
+            let a = message.content.split(' ');
+
+            if (a.length !== 2){
+                message.channel.send('Please use the proper format for this command\nIf you need help, message !h to get the list of commands')
+
+            } else {
+
+                if (a[1] in tasks[id]){
+                    message.channel.send('Task Already Exists!')
+
+                } else {
+                    tasks[id][a[1]] = "None"
+                    message.channel.send('Task Added!')
+                }
+            }
+        }
+
+        else if (message.content.startsWith('!am ')) {
+
+            let a = message.content.split(' ');
+
+            if (a.length !== 3){
+                message.channel.send('Please use the proper format for this command\nIf you need help, message !h to get the list of commands')
+
+            } else {
+
+                if (a[1] in tasks[id]){
+                    message.channel.send('Task Already Exists!')
+
+                } else if(Number(a[2]) > 0) {
+                    tasks[id][a[1]] = 'None' //fix this
+                    message.channel.send('Task Added!')
+                    
+                    let today = new Date()
+                    let date = new Date(today.getFullYear(), today.getMonth(), today.getDate(), today.getHours(), today.getMinutes()+Number(a[2]))
+
+                    jobs.push(schedule.scheduleJob(date, function(){
+                        message.channel.send(`<@${message.author.id}>`)
+                        message.channel.send(`Task: **${a[1]}**`)
+                    }))
+                    
+                } else {
+                    message.channel.send('Please enter an integer greater than or equal to 1 minute')
+                }
+            }
+        }
+
+
         else if (message.content.startsWith('!d ')) {
 
             let a = message.content.split(' ');
@@ -59,21 +130,27 @@ client.on('messageCreate', (message) => {
                 message.channel.send('You have no tasks right now.')
 
             } else {
-                message.channel.send('Tasks:')
+                message.channel.send(`Tasks(${Object.keys(tasks[id]).length}):`)
 
                 for (var task in tasks[id]){
-                    message.channel.send(`${task} @ ${tasks[id][task]}`)
+                    if (tasks[id][task] !== "None"){
+                        message.channel.send(`${task} @ ${tasks[id][task]}`)
+                    } else {
+                        message.channel.send(`${task}`)
+                    }
+                    
                 } 
             }
             
         } else if (message.content.startsWith('!h')) {
-            message.channel.send('Commands:\n!a <task> <time> | Add a new task add a certain time\n!d <task> | Delete a task\n!l | List all current tasks\n!h | View command list')
+            message.channel.send('Commands:\n**!a** <task> <time> | Add a new task at a certain time\n**!as** <task> | Add a new task without a time\n**!am** <minutes> | Add a task in specified minutes\n**!d** <task> | Delete a task\n**!l** | List all current tasks\n**!h** | View command list')
 
         } else {
             message.channel.send(`${message.content} is not a proper command. Message !h for help with commands.`)
         }
-
     }
 });
 
+
 client.login(process.env.TOKEN)
+
